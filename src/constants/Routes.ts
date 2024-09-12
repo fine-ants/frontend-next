65610ab9dde6244abc89438df812aaa72bd94516
Jay <chlwodnjs34@gmail.com>
2024-09-12 17:57:49 +0900

    #1 feat: 상수화된 Route, Style 값 추가

---
+++ src/constants/Routes.ts
@@ -0,0 +1,35 @@
+// 경로를 변수에 할당
+const Routes = {
+  LANDING: "/",
+  SIGNUP: "/signup",
+  SIGNIN: "/signin",
+  OAUTHLOADING: "/signin/loading",
+  PROFILE: (tab?: "profile" | "account") => {
+    return tab ? `/settings/${tab}` : "/settings/:tab";
+  },
+  NOTIFICATIONS: (tab?: "stock" | "portfolio") => {
+    return tab ? `/notifications/${tab}` : "/notifications/:tab";
+  },
+  DASHBOARD: "/dashboard",
+  WATCHLISTS: "/watchlists",
+  WATCHLIST: (watchlistId?: number | string) => {
+    return watchlistId
+      ? `/watchlists/${watchlistId}`
+      : "/watchlists/:watchlistId";
+  },
+  PORTFOLIOS: "/portfolios",
+  PORTFOLIO: (portfolioId?: number | string) => {
+    return portfolioId
+      ? `/portfolio/${portfolioId}`
+      : "/portfolio/:portfolioId";
+  },
+  INDICES: (tickerSymbol?: string) => {
+    return tickerSymbol ? `/indices/${tickerSymbol}` : "/indices/:tickerSymbol";
+  },
+  STOCK: (tickerSymbol?: string) => {
+    return tickerSymbol ? `/stock/${tickerSymbol}` : "/stock/:tickerSymbol";
+  },
+  FALLBACK: "*",
+};
+
+export default Routes;
