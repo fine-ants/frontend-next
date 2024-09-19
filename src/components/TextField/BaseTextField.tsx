00dbd3a52d65749d56b6b1a97280fb84f559d628
Jay <chlwodnjs34@gmail.com>
2024-09-19 15:35:31 +0900

    #4 feat: TextField 관련 컴포넌트들 추가

---
+++ src/components/TextField/BaseTextField.tsx
@@ -0,0 +1,75 @@
+import designSystem from "@/styles/designSystem";
+import { OutlinedInput, OutlinedInputProps } from "@mui/material";
+import styled from "styled-components";
+
+export type Size = "h24" | "h32" | "h44" | "h48";
+
+type Props = {
+  size: Size;
+} & Omit<OutlinedInputProps, "size">;
+
+export function BaseTextField({ size, ...rest }: Props) {
+  return <TextField {...rest} $size={size} />;
+}
+
+const INPUT_SIZE_TO_CSS = {
+  h24: `
+    height: 24px;
+    padding: 0px 8px;
+    border-radius: 2px;
+  `,
+  h32: `
+      height: 32px;
+      padding: 4px 0;
+      border-radius: 3px;
+  `,
+  h44: `
+    height: 44px;
+    padding: 8px 0;
+    border-radius: 4px;
+  `,
+  h48: `
+      height: 48px;
+      padding: 13.5px 0;
+      border-radius: 4px;
+  `,
+};
+
+const TEXT_FIELD_SIZE_TO_CSS = {
+  h24: "padding: 0 8px;",
+  h32: "padding: 0 8px;",
+  h44: "padding: 0 12px;",
+  h48: "padding: 0 12px;",
+};
+
+const TextField = styled(OutlinedInput)<{ $size: Size }>`
+  width: 100%;
+  border: none;
+  background: ${designSystem.color.neutral.white};
+  ${({ $size }) => TEXT_FIELD_SIZE_TO_CSS[$size]}
+
+  .MuiInputAdornment-positionStart {
+    margin-right: 3px;
+  }
+
+  .MuiOutlinedInput-input {
+    width: 100%;
+    ${({ $size }) => INPUT_SIZE_TO_CSS[$size]}
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    box-sizing: border-box;
+    background: ${designSystem.color.neutral.white};
+    ${designSystem.font.body3};
+  }
+
+  &.Mui-focused .MuiOutlinedInput-notchedOutline,
+  &:hover .MuiOutlinedInput-notchedOutline,
+  .MuiOutlinedInput-notchedOutline {
+    border: 1px solid ${designSystem.color.neutral.gray200};
+  }
+
+  &.Mui-error .MuiOutlinedInput-notchedOutline {
+    border: 1px solid ${designSystem.color.state.red500};
+  }
+`;
---
+++ src/components/TextField/TextField.tsx
@@ -0,0 +1,98 @@
+import designSystem from "@/styles/designSystem";
+import { useBoolean } from "@fineants/demolition";
+import { InputAdornment } from "@mui/material";
+import { ChangeEvent, HTMLInputTypeAttribute, ReactNode } from "react";
+import styled from "styled-components";
+import { IconButton } from "../Buttons/IconButton";
+import { BaseTextField, Size } from "./BaseTextField";
+import { ErrorText } from "./ErrorText";
+
+type Props = {
+  id?: string;
+  size?: Size;
+  error?: boolean;
+  errorText?: string;
+  placeholder?: string;
+  startAdornment?: ReactNode;
+  endAdornment?: ReactNode;
+  disabled?: boolean;
+  value: string;
+  type?: HTMLInputTypeAttribute;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  clearValue?: () => void;
+};
+
+export function TextField({
+  id,
+  size = "h44",
+  error,
+  errorText,
+  placeholder,
+  startAdornment,
+  endAdornment,
+  disabled = false,
+  value,
+  type = "text",
+  onChange,
+  clearValue,
+}: Props) {
+  const {
+    state: isFocused,
+    setTrue: handleFocus,
+    setFalse: handleBlur,
+  } = useBoolean();
+
+  const isError = value !== "" && error;
+
+  return (
+    <StyledTextFieldWrapper>
+      <BaseTextField
+        type={type}
+        id={id ?? undefined}
+        size={size}
+        error={isError}
+        value={value}
+        onChange={onChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        placeholder={placeholder}
+        disabled={disabled}
+        autoCapitalize="none"
+        startAdornment={
+          startAdornment && (
+            <InputAdornment position="start" sx={startAdornmentSx}>
+              {startAdornment}
+            </InputAdornment>
+          )
+        }
+        endAdornment={
+          <InputAdornment position="end">{endAdornment}</InputAdornment> ??
+          (isFocused && (
+            <InputAdornment position="end">
+              <IconButton
+                icon="close"
+                size="h24"
+                iconColor="custom"
+                customColor={{
+                  color: "gray600",
+                  hoverColor: "gray50",
+                }}
+                onClick={clearValue}
+              />
+            </InputAdornment>
+          ))
+        }
+      />
+      {isError && errorText && <ErrorText>{errorText}</ErrorText>}
+    </StyledTextFieldWrapper>
+  );
+}
+
+const StyledTextFieldWrapper = styled.div`
+  width: 100%;
+  position: relative;
+`;
+
+const startAdornmentSx = {
+  color: designSystem.color.neutral.gray600,
+};
