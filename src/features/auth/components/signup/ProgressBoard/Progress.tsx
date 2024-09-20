98811416cee38920f68dd26ce240f05ce69495ce
Jay <chlwodnjs34@gmail.com>
2024-09-20 15:59:01 +0900

    #6 feat: 회원가입 프로그레스 컴포넌트 및 constants 추가

---
+++ src/features/auth/components/signup/ProgressBoard/Progress.tsx
@@ -0,0 +1,138 @@
+import useResponsiveLayout from "@/hooks/useResponsiveLayout";
+import designSystem from "@/styles/designSystem";
+import styled from "styled-components";
+
+type ProgressProps = {
+  stepNumber: number;
+  title: string;
+  isLast: boolean;
+  currentStepNumber: number;
+};
+
+export function Progress({
+  stepNumber,
+  title,
+  isLast,
+  currentStepNumber,
+}: ProgressProps) {
+  const { isDesktop } = useResponsiveLayout();
+
+  return (
+    <StyledProgress>
+      <ProgressContent $isDesktop={isDesktop}>
+        <ProgressStepNumber
+          $currentStepNumber={currentStepNumber}
+          $stepNumber={stepNumber}>
+          {stepNumber}
+        </ProgressStepNumber>
+        {isDesktop && (
+          <ProgressTitle
+            $currentStepNumber={currentStepNumber}
+            $stepNumber={stepNumber}>
+            {title}
+          </ProgressTitle>
+        )}
+      </ProgressContent>
+      {!isLast && isDesktop && (
+        <Dots>
+          {Array.from({ length: 3 }).map((_, index) => (
+            <Dot
+              key={index}
+              $currentIndex={currentStepNumber}
+              $progressIndex={stepNumber}
+            />
+          ))}
+        </Dots>
+      )}
+    </StyledProgress>
+  );
+}
+
+const getBackgroundStyle = (currentIndex: number, progressIndex: number) => {
+  if (currentIndex === progressIndex) {
+    return designSystem.color.primary.blue500;
+  } else if (currentIndex < progressIndex) {
+    return designSystem.color.neutral.gray400;
+  } else {
+    return designSystem.color.primary.blue200;
+  }
+};
+
+const getColorStyle = (currentIndex: number, progressIndex: number) => {
+  if (currentIndex === progressIndex) {
+    return designSystem.color.primary.blue500;
+  } else if (currentIndex < progressIndex) {
+    return designSystem.color.neutral.gray600;
+  } else {
+    return designSystem.color.primary.blue300;
+  }
+};
+
+const StyledProgress = styled.div`
+  display: flex;
+`;
+
+const ProgressContent = styled.div<{ $isDesktop: boolean }>`
+  width: ${({ $isDesktop }) => ($isDesktop ? "102px" : "auto")};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+`;
+
+const ProgressStepNumber = styled.div<{
+  $currentStepNumber: number;
+  $stepNumber: number;
+}>`
+  display: flex;
+  width: 24px;
+  height: 24px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 12px;
+  background: ${({
+    $currentStepNumber: $currentIndex,
+    $stepNumber: $progressIndex,
+  }) => getBackgroundStyle($currentIndex, $progressIndex)};
+  color: ${designSystem.color.neutral.white};
+  text-align: center;
+  font-family: "IBM Plex Sans";
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 14px;
+  letter-spacing: -0.24px;
+`;
+
+const ProgressTitle = styled.div<{
+  $currentStepNumber: number;
+  $stepNumber: number;
+}>`
+  ${designSystem.font.title6};
+  color: ${({
+    $currentStepNumber: $currentIndex,
+    $stepNumber: $progressIndex,
+  }) => getColorStyle($currentIndex, $progressIndex)};
+  text-align: center;
+`;
+
+const Dots = styled.div`
+  width: 24px;
+  height: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+`;
+
+const Dot = styled.div<{ $currentIndex: number; $progressIndex: number }>`
+  width: 4px;
+  height: 4px;
+  flex-shrink: 0;
+  background: ${({ $currentIndex, $progressIndex }) =>
+    getBackgroundStyle($currentIndex, $progressIndex)};
+  border-radius: 50%;
+`;
