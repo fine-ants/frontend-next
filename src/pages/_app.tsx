d8153905998460a83f751ff7daa5d01247a8ab3c
Jay <chlwodnjs34@gmail.com>
2024-09-25 16:28:30 +0900

    #8 feat: QueryClientProvider 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import GlobalStyles from "@/styles/GlobalStyles";
 import { AppCacheProvider } from "@mui/material-nextjs/v13-pagesRouter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import localFont from "next/font/local";
 
@@ -28,13 +29,51 @@ const ibmPlexSansKR = localFont({
   ],
 });
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+
+  // TODO : api 환경 설정 끝나고 toast 컴포넌트와 함께 적용하기
+  // queryCache: new QueryCache({
+  //   onError: (_, query) => {
+  //     if (query.meta?.toastErrorMessage) {
+  //       toast.error(query.meta.toastErrorMessage as string);
+  //       return;
+  //     }
+  //   },
+  // }),
+  // mutationCache: new MutationCache({
+  //   onSuccess: (_, __, ___, mutation) => {
+  //     if (mutation.meta?.toastSuccessMessage) {
+  //       toast.success(mutation.meta.toastSuccessMessage as string);
+  //       return;
+  //     }
+  //   },
+  //   onError: (_, __, ___, mutation) => {
+  //     if (mutation.meta?.toastErrorMessage) {
+  //       toast.error(mutation.meta.toastErrorMessage as string);
+  //       return;
+  //     }
+  //   },
+  // }),
+});
+const a =
+  process.env.NODE_ENV === "production"
+    ? process.env.NEXT_CLIENT_URL_PROD
+    : process.env.NEXT_CLIENT_URL_DEV;
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AppCacheProvider>
-      <main className={ibmPlexSansKR.className}>
-        <GlobalStyles />
-        <Component {...pageProps} class />
-      </main>
-    </AppCacheProvider>
+    <QueryClientProvider client={queryClient}>
+      <AppCacheProvider>
+        <main className={ibmPlexSansKR.className}>
+          <GlobalStyles />
+          <Component {...pageProps} class />
+        </main>
+      </AppCacheProvider>
+    </QueryClientProvider>
   );
 }
