6567338f59731871223e51dc3a71dc3780140bff
Jay <chlwodnjs34@gmail.com>
2024-10-15 16:01:28 +0900

    #12 feat: Dashboard api 추가

---
+++ src/features/dashboard/api/queries/queryKeys.ts
@@ -0,0 +1,7 @@
+import { createQueryKeys } from "@lukemorales/query-key-factory";
+
+export const dashboardKeys = createQueryKeys("dashboard", {
+  overview: null,
+  pieChart: null,
+  lineChart: null,
+});
