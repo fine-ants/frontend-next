bfe19eb38cd3b62d0517ec7c3f449b2d6a602b7f
Jay <chlwodnjs34@gmail.com>
2024-10-17 13:33:07 +0900

    #12 feat: logout proxy api 추가

---
+++ src/pages/api/proxy/auth/logout.ts
@@ -0,0 +1,59 @@
+import { proxyFetcher } from "@/api/fetcher";
+import { Response } from "@/api/types";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const replaceCookieDomain = (cookieStr: string, newDomain: string) => {
+  const cookieParts = cookieStr.split(";");
+  const updatedCookieParts = cookieParts.map((part) => {
+    if (part.trim().startsWith("Domain=")) {
+      return ` Domain=${newDomain}`;
+    }
+    return part;
+  });
+  return updatedCookieParts.join(";");
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    try {
+      const cookies = req.cookies;
+      const cookieString = Object.entries(cookies)
+        .map(([key, value]) => `${key}=${value}`)
+        .join("; ");
+
+      const { data, cookies: { accessToken, refreshToken } = {} } =
+        await proxyFetcher.post<Response<null>>("/auth/logout", req.body, {
+          headers: {
+            Cookie: cookieString || "",
+          },
+        });
+
+      if (accessToken && refreshToken) {
+        const updatedAccessToken = replaceCookieDomain(
+          accessToken,
+          "localhost"
+        );
+        const updatedRefreshToken = replaceCookieDomain(
+          refreshToken,
+          "localhost"
+        );
+
+        console.log(updatedAccessToken);
+        console.log(updatedRefreshToken);
+        console.log("token");
+
+        res.setHeader("Set-Cookie", [updatedAccessToken, updatedRefreshToken]);
+      }
+
+      return res.status(200).json(data);
+    } catch (error) {
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
