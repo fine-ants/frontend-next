0b1ae94942f1394c60d1e455942303db43a4439c
Jay <chlwodnjs34@gmail.com>
2024-10-21 13:28:41 +0900

    #14 refactor: 의도한 기능이 제대로 작동하지 않아 우선 기존의 기본 endAdornment 삭제

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -3,7 +3,6 @@ import { useBoolean } from "@fineants/demolition";
 import { InputAdornment } from "@mui/material";
 import { ChangeEvent, HTMLInputTypeAttribute, ReactNode } from "react";
 import styled from "styled-components";
-import { IconButton } from "../Buttons/IconButton";
 import { BaseTextField, Size } from "./BaseTextField";
 import { ErrorText } from "./ErrorText";
 
@@ -69,20 +68,7 @@ export function TextField({
           endAdornment ? (
             <InputAdornment position="end">{endAdornment}</InputAdornment>
           ) : (
-            isFocused && (
-              <InputAdornment position="end">
-                <IconButton
-                  icon="close"
-                  size="h24"
-                  iconColor="custom"
-                  customColor={{
-                    color: "gray600",
-                    hoverColor: "gray50",
-                  }}
-                  onClick={clearValue}
-                />
-              </InputAdornment>
-            )
+            <></>
           )
         }
       />
