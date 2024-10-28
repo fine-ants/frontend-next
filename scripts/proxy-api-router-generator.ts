394e485b5fbc964053efb6f178c0c99306e91de1
Jay <chlwodnjs34@gmail.com>
2024-10-28 15:41:21 +0900

    #16 refactor: 초기 실행시 초기화 로직 추가 및 index.ts 삭제 대응 가능하도록 수정

--- scripts/proxy-api-router-generator.ts
+++ scripts/proxy-api-router-generator.ts
@@ -1,8 +1,54 @@
 import chokidar from "chokidar";
+import fs from "fs";
 import path from "path";
 import { generateApiRouter } from "./utils/api-router-generator-utils";
 
 const watchPath = path.resolve("./src/features");
+const proxyPath = path.resolve("./src/pages/api/proxy");
+
+// ./src/features/api 아래에 있는 index.ts 체크
+function isApiIndexFile(filePath: string): boolean {
+  return filePath.endsWith(path.join("api", "index.ts"));
+}
+
+// 초기화: ./src/pages/api/proxy 경로 삭제 후 생성
+function initializeProxyPath() {
+  fs.rmSync(proxyPath, { recursive: true, force: true });
+  fs.mkdirSync(proxyPath, { recursive: true }); // proxyPath를 재생성
+
+  console.log(`Generator: Cleared and recreated directory: ${proxyPath}`);
+}
+
+// 전체 재생성: watchPath 내부의 모든 index.ts 파일을 다시 생성
+function regenerateAllApiRouters() {
+  const files = getAllIndexFiles(watchPath);
+
+  files.forEach((filePath) => {
+    console.log(`Generator: Regenerating API router for: ${filePath}`);
+    generateApiRouter(filePath);
+  });
+}
+
+// watchPath 내부의 모든 index.ts 파일 경로를 가져오는 함수
+function getAllIndexFiles(dir: string): string[] {
+  let results: string[] = [];
+  const list = fs.readdirSync(dir);
+
+  list.forEach((file) => {
+    const filePath = path.resolve(dir, file);
+    const stat = fs.statSync(filePath);
+
+    if (stat && stat.isDirectory()) {
+      results = results.concat(getAllIndexFiles(filePath));
+    } else if (isApiIndexFile(filePath)) {
+      results.push(filePath);
+    }
+  });
+  return results;
+}
+
+// 초기화 함수 실행
+initializeProxyPath();
 
 // chokidar 설정: 파일 변경 감시
 const watcher = chokidar.watch(watchPath, {
@@ -21,16 +67,32 @@ watcher.on("ready", () => {
 
 // 파일 변경
 watcher.on("change", (filePath: string) => {
-  if (filePath.endsWith(path.join("api", "index.ts"))) {
+  if (isApiIndexFile(filePath)) {
     console.log(`Generator: File changed: ${filePath}`);
+
     generateApiRouter(filePath);
   }
 });
 
 // 파일 추가
 watcher.on("add", (filePath: string) => {
-  if (filePath.endsWith(path.join("api", "index.ts"))) {
+  if (isApiIndexFile(filePath)) {
     console.log(`Generator: File added: ${filePath}`);
+
     generateApiRouter(filePath);
+  }
+});
+
+// 파일 삭제
+watcher.on("unlink", (filePath: string) => {
+  if (isApiIndexFile(filePath)) {
+    console.log(`Generator: File deleted: ${filePath}`);
+
+    initializeProxyPath();
+    regenerateAllApiRouters();
+
+    console.log(
+      "Generator: Re-initialized proxy path and regenerated all API routers due to deletion."
+    );
   }
 });
