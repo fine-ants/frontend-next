e426f89f85250a8b9c8e5867d56df83e51a7c845
Jay <chlwodnjs34@gmail.com>
2024-11-21 20:34:25 +0900

    #22 refactor: user 사용처 변경

--- src/features/user/components/desktop/UserControls.tsx
+++ src/features/user/components/desktop/UserControls.tsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import UserDropdown from "./UserDropdown";
 
-// export default function UserControls({ user }: { user: User }) {
 export default function UserControls() {
   return (
     <StyledUserControls>
