2b86997d4a404b5128778f190b4a5a9942a36e71
Jay <chlwodnjs34@gmail.com>
2024-11-21 14:31:37 +0900

    #22 refactor: staleTime 삭제

--- src/features/auth/api/queries/useAuthStatusQuery.ts
+++ src/features/auth/api/queries/useAuthStatusQuery.ts
@@ -8,8 +8,6 @@ export default function useAuthStatusQuery(cookies?: Record<string, string>) {
     queryFn: () => getAuthStatus(cookies),
     select: (res) => res.data,
     retry: 0,
-    // TODO : 쿠키 만료시간에 맞게 추후에 수정 필요
     gcTime: 1000 * 60 * 5,
-    staleTime: 1000 * 60 * 5,
   });
 }
