cf1354c2f37f03409ae0d0a068e76a11ba6bd174
Jay <chlwodnjs34@gmail.com>
2024-11-27 15:35:58 +0900

    #24 feat: Portfolio Overview 관련 컴포넌트 추가

---
+++ src/features/portfolio/components/portfolioOverview/desktop/PortfolioOverviewBodyD.tsx
@@ -0,0 +1,375 @@
+import RateBadge from "@/components/Badges/RateBadge";
+import { IconButton } from "@/components/Buttons/IconButton";
+import { Icon } from "@/components/Icon";
+import ConditionalTooltip from "@/components/Tooltips/ConditionalTooltip";
+import { CustomTooltip } from "@/components/Tooltips/CustomTooltip";
+import { PortfolioDetails } from "@/features/portfolio/api/types";
+import { usePortfolioId } from "@/features/portfolio/hook/usePortfolioId";
+import designSystem from "@/styles/designSystem";
+import { thousandsDelimiter } from "@fineants/demolition";
+import { debounce } from "@mui/material";
+import { memo, useCallback } from "react";
+import styled from "styled-components";
+import RealtimeValue from "../../RealtimeValue";
+import { TargetGainToolTip } from "../TargetGainToolTip";
+
+type Props = {
+  data: Omit<
+    PortfolioDetails,
+    "id" | "name" | "securitiesFirm" | "currentValuation"
+  >;
+};
+
+export default memo(function PortfolioOverviewBodyD({ data }: Props) {
+  const {
+    budget,
+    investedAmount,
+    balance,
+    provisionalLossBalance,
+    targetGain,
+    targetReturnRate,
+    targetGainNotify,
+    maximumLoss,
+    maximumLossRate,
+    maxLossNotify,
+    totalGain,
+    totalGainRate,
+    dailyGain,
+    dailyGainRate,
+    annualDividend,
+    annualDividendYield,
+    annualInvestmentDividendYield,
+  } = data;
+
+  const portfolioId = usePortfolioId();
+
+  // TODO: 알림 추가하면서 추가하기
+  // const { mutate } = usePortfolioNotificationSettingsMutation(
+  //   Number(portfolioId)
+  // );
+
+  const onTargetGainNotifyButtonClick = useCallback(
+    debounce(() => {
+      // mutate({
+      //   notificationType: "targetGain",
+      //   body: { isActive: !targetGainNotify },
+      // });
+    }, 250),
+    []
+  );
+
+  const onMaxLossNotifyButtonClick = useCallback(
+    debounce(() => {
+      // mutate({
+      //   notificationType: "maxLoss",
+      //   body: { isActive: !maxLossNotify },
+      // });
+    }, 250),
+    []
+  );
+
+  return (
+    <StyledOverviewBody>
+      <BudgetSection
+        budget={budget}
+        investedAmount={investedAmount}
+        balance={balance}
+        provisionalLossBalance={provisionalLossBalance}
+      />
+      <TargetAndLossSection
+        targetGain={targetGain}
+        targetReturnRate={targetReturnRate}
+        targetGainNotify={targetGainNotify}
+        maximumLoss={maximumLoss}
+        maximumLossRate={maximumLossRate}
+        maxLossNotify={maxLossNotify}
+        onTargetGainNotifyButtonClick={onTargetGainNotifyButtonClick}
+        onMaxLossNotifyButtonClick={onMaxLossNotifyButtonClick}
+      />
+      <GainAndLossSection
+        totalGain={totalGain}
+        totalGainRate={totalGainRate}
+        dailyGain={dailyGain}
+        dailyGainRate={dailyGainRate}
+      />
+      <DividendSection
+        annualDividend={annualDividend}
+        annualDividendYield={annualDividendYield}
+        annualInvestmentDividendYield={annualInvestmentDividendYield}
+      />
+    </StyledOverviewBody>
+  );
+});
+
+const BudgetSection = memo(function ({
+  budget,
+  investedAmount,
+  balance,
+  provisionalLossBalance,
+}: Pick<
+  PortfolioDetails,
+  "budget" | "investedAmount" | "balance" | "provisionalLossBalance"
+>) {
+  return (
+    <OverviewBodySection>
+      <OverviewBodyData>
+        <div>예산</div>
+        <span>{thousandsDelimiter(budget)}</span>
+      </OverviewBodyData>
+      <OverviewBodyData>
+        <div>투자금액</div>
+        <span>{thousandsDelimiter(investedAmount)}</span>
+      </OverviewBodyData>
+      <OverviewBodyData>
+        <div>잔고</div>
+        <span>{thousandsDelimiter(balance)}</span>
+      </OverviewBodyData>
+      <OverviewBodyData>
+        <CustomTooltip
+          placement="bottom-start"
+          title={
+            <p>
+              손실 종목을 매도 시 현재 잔고의 감당력을 표현하는 것으로 매도 후
+              실제 잔고를 나타내는 것이 아닙니다
+              <br />
+              잔고 - 손실 종목의 손실 합
+            </p>
+          }>
+          <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+            잠정 손실 잔고 <Icon icon="help" size={16} color="gray400" />
+          </div>
+        </CustomTooltip>
+        <span>{thousandsDelimiter(provisionalLossBalance)}</span>
+      </OverviewBodyData>
+    </OverviewBodySection>
+  );
+});
+
+const TargetAndLossSection = memo(function ({
+  targetGain,
+  targetReturnRate,
+  targetGainNotify,
+  maximumLoss,
+  maximumLossRate,
+  maxLossNotify,
+  onTargetGainNotifyButtonClick,
+  onMaxLossNotifyButtonClick,
+}: Pick<
+  PortfolioDetails,
+  | "targetGain"
+  | "targetReturnRate"
+  | "targetGainNotify"
+  | "maximumLoss"
+  | "maximumLossRate"
+  | "maxLossNotify"
+> & {
+  onTargetGainNotifyButtonClick: () => void;
+  onMaxLossNotifyButtonClick: () => void;
+}) {
+  return (
+    <OverviewBodySection>
+      <OverviewBodyData>
+        <NotificationLabel>
+          목표 수익률
+          <TargetGainToolTip
+            targetGain={targetGain}
+            targetGainNotify={targetGainNotify}
+            disabled={targetGain === 0}
+            onClick={onTargetGainNotifyButtonClick}
+          />
+        </NotificationLabel>
+        <span>{targetGain === 0 ? "-" : thousandsDelimiter(targetGain)}</span>
+      </OverviewBodyData>
+      <div style={{ marginLeft: "auto" }}>
+        {targetGain === 0 ? (
+          "-"
+        ) : (
+          <RateBadge
+            size={16}
+            value={targetReturnRate}
+            bgColorStatus={false}
+            iconStatus={false}
+          />
+        )}
+      </div>
+      <OverviewBodyData>
+        <NotificationLabel>
+          최대 손실률
+          <ConditionalTooltip
+            condition={maximumLoss !== 0}
+            title={"포트폴리오 최대 손실률을 먼저 설정해주세요"}
+            placement={"bottom-start"}>
+            <div>
+              <IconButton
+                icon="notification"
+                size="h24"
+                iconColor="custom"
+                customColor={{
+                  color: maxLossNotify ? "blue500" : "gray400",
+                  hoverColor: "gray50",
+                }}
+                disabled={maximumLoss === 0}
+                onClick={onMaxLossNotifyButtonClick}
+                aria-label="최대 손실률 알림 설정 토글"
+              />
+            </div>
+          </ConditionalTooltip>
+        </NotificationLabel>
+        <span>{maximumLoss === 0 ? "-" : thousandsDelimiter(maximumLoss)}</span>
+      </OverviewBodyData>
+      <div style={{ marginLeft: "auto" }}>
+        {maximumLoss === 0 ? (
+          "-"
+        ) : (
+          <RateBadge
+            size={16}
+            value={-maximumLossRate}
+            bgColorStatus={false}
+            iconStatus={false}
+          />
+        )}
+      </div>
+    </OverviewBodySection>
+  );
+});
+
+const GainAndLossSection = memo(function ({
+  totalGain,
+  totalGainRate,
+  dailyGain,
+  dailyGainRate,
+}: Pick<
+  PortfolioDetails,
+  "totalGain" | "totalGainRate" | "dailyGain" | "dailyGainRate"
+>) {
+  return (
+    <OverviewBodySection>
+      <OverviewBodyData>
+        <div>총 손익</div>
+        <RealtimeValue value={totalGain} />
+      </OverviewBodyData>
+      <div style={{ marginLeft: "auto" }}>
+        <RateBadge
+          size={16}
+          value={totalGainRate}
+          bgColorStatus={false}
+          iconStatus={false}
+        />
+      </div>
+      <OverviewBodyData>
+        <div>당일 손익</div>
+        <RealtimeValue value={dailyGain} />
+      </OverviewBodyData>
+      <div style={{ marginLeft: "auto" }}>
+        <RateBadge
+          size={16}
+          value={dailyGainRate}
+          bgColorStatus={false}
+          iconStatus={false}
+        />
+      </div>
+    </OverviewBodySection>
+  );
+});
+
+const DividendSection = memo(function ({
+  annualDividend,
+  annualDividendYield,
+  annualInvestmentDividendYield,
+}: Pick<
+  PortfolioDetails,
+  "annualDividend" | "annualDividendYield" | "annualInvestmentDividendYield"
+>) {
+  return (
+    <OverviewBodySection>
+      <OverviewBodyData>
+        <div>총 연배당금</div>
+        <span>{thousandsDelimiter(annualDividend)}</span>
+      </OverviewBodyData>
+      <div style={{ marginLeft: "auto" }}>
+        <RateBadge
+          size={16}
+          value={annualDividendYield}
+          bgColorStatus={false}
+          iconStatus={false}
+        />
+      </div>
+      <OverviewBodyData>
+        <CustomTooltip
+          placement="bottom-start"
+          title={
+            <p style={{ font: designSystem.font.body4 }}>
+              총 투자금액 대비 연배당률
+              <br />연 배당금 / 투자금액 * 100%
+            </p>
+          }>
+          <div style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+            투자대비 연 배당률
+            <Icon icon="help" size={16} color="gray400" />
+          </div>
+        </CustomTooltip>
+        <RateBadge
+          size={16}
+          value={annualInvestmentDividendYield}
+          bgColorStatus={false}
+          iconStatus={false}
+        />
+      </OverviewBodyData>
+    </OverviewBodySection>
+  );
+});
+
+const StyledOverviewBody = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 1px;
+  border: 1px solid ${designSystem.color.neutral.gray100};
+  border-radius: 8px;
+  overflow: hidden;
+
+  & > div {
+    display: flex;
+    flex-direction: column;
+    padding: 16px;
+  }
+
+  & > div:nth-child(odd) {
+    border-right: 1px solid ${designSystem.color.neutral.gray100};
+  }
+
+  & > div:nth-child(-n + 2) {
+    border-bottom: 1px solid ${designSystem.color.neutral.gray100};
+  }
+`;
+
+const OverviewBodySection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  width: 444px;
+  height: 140px;
+  padding: 16px;
+
+  &:first-child {
+    border-right: 1px solid ${designSystem.color.neutral.gray100};
+  }
+`;
+
+const OverviewBodyData = styled.div`
+  height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  > span {
+    ${designSystem.font.body3};
+    color: ${designSystem.color.neutral.gray900};
+  }
+`;
+
+const NotificationLabel = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+`;
---
+++ src/features/portfolio/components/portfolioOverview/desktop/PortfolioOverviewD.tsx
@@ -0,0 +1,206 @@
+import LabelBadge from "@/components/Badges/LabelBadge";
+import Breadcrumb from "@/components/Breadcrumb";
+import Button from "@/components/Buttons/Button";
+import { Icon } from "@/components/Icon";
+import Routes from "@/constants/Routes";
+import { securitiesFirmLogos } from "@/constants/securitiesFirm";
+import { PortfolioDetails } from "@/features/portfolio/api/types";
+import designSystem from "@/styles/designSystem";
+import { thousandsDelimiter, useBoolean } from "@fineants/demolition";
+import { useRouter } from "next/router";
+import { memo } from "react";
+import styled from "styled-components";
+import PortfolioOverviewBodyD from "./PortfolioOverviewBodyD";
+
+type Props = {
+  data: PortfolioDetails;
+};
+
+export default memo(function PortfolioOverviewD({ data }: Props) {
+  // const navigate = useNavigate();
+  const router = useRouter();
+  // const { portfolioId } = useParams();
+  // const { mutate: portfolioDeleteMutate } = usePortfolioDeleteMutation();
+
+  const { id, name, securitiesFirm, currentValuation, ...overViewData } = data;
+
+  const {
+    state: isDialogOpen,
+    setTrue: onPortfolioEdit,
+    setFalse: onDialogClose,
+  } = useBoolean();
+  const {
+    state: isConfirmOpen,
+    setTrue: onPortfolioRemove,
+    setFalse: onConfirmAlertClose,
+  } = useBoolean();
+
+  const onConfirmAction = () => {
+    // portfolioDeleteMutate(Number(portfolioId));
+    router.push(Routes.PORTFOLIOS);
+  };
+
+  return (
+    <StyledPortfolioOverview>
+      <Header
+        id={id}
+        name={name}
+        securitiesFirm={securitiesFirm}
+        onPortfolioRemove={onPortfolioRemove}
+        onPortfolioEdit={onPortfolioEdit}
+      />
+
+      <CurrentValue currentValuation={currentValuation} />
+
+      <PortfolioOverviewBodyD data={overViewData} />
+
+      {/* {isDialogOpen && (
+        <PortfolioEditDialog
+          isOpen={isDialogOpen}
+          onClose={onDialogClose}
+          portfolioDetails={data}
+        />
+      )}
+      {isConfirmOpen && (
+        <PortfolioDeleteConfirm
+          isOpen={isConfirmOpen}
+          portfolioName={data.name}
+          onClose={onConfirmAlertClose}
+          onConfirm={onConfirmAction}
+        />
+      )} */}
+    </StyledPortfolioOverview>
+  );
+});
+
+type HeaderProps = Pick<PortfolioDetails, "name" | "id" | "securitiesFirm"> & {
+  onPortfolioRemove: () => void;
+  onPortfolioEdit: () => void;
+};
+
+const Header = memo(function ({
+  name,
+  id,
+  securitiesFirm,
+  onPortfolioRemove,
+  onPortfolioEdit,
+}: HeaderProps) {
+  return (
+    <PortfolioOverviewHead>
+      <Breadcrumb
+        depthData={[
+          { name: "내 포트폴리오", url: Routes.PORTFOLIOS },
+          { name, url: Routes.PORTFOLIO(id) },
+        ]}
+      />
+      <TitleContent>
+        <TitleWrapper>
+          <FirmImage
+            src={securitiesFirmLogos[securitiesFirm].src}
+            alt={`${securitiesFirm} 로고`}
+          />
+          <Title>{name}</Title>
+          <LabelBadge title={securitiesFirm} />
+        </TitleWrapper>
+        <ButtonsWrapper>
+          <Button
+            variant="tertiary"
+            size="h32"
+            onClick={onPortfolioRemove}
+            disabled={false}>
+            <Icon icon="trash" size={16} color="gray600" />
+            삭제
+          </Button>
+          <Button
+            variant="secondary"
+            size="h32"
+            onClick={onPortfolioEdit}
+            disabled={false}>
+            <Icon icon="edit" size={16} color="blue500" />
+            편집
+          </Button>
+        </ButtonsWrapper>
+      </TitleContent>
+    </PortfolioOverviewHead>
+  );
+});
+
+const CurrentValue = memo(function ({
+  currentValuation,
+}: Pick<PortfolioDetails, "currentValuation">) {
+  return (
+    <ValuationContainer>
+      <div>평가금액</div>
+      <CurrentValuation>
+        ₩<span>{thousandsDelimiter(currentValuation)}</span>
+      </CurrentValuation>
+    </ValuationContainer>
+  );
+});
+
+const StyledPortfolioOverview = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  width: 100%;
+`;
+
+const PortfolioOverviewHead = styled.div`
+  height: 73px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const TitleContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+const FirmImage = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  overflow: hidden;
+`;
+
+const Title = styled.span`
+  ${designSystem.font.heading3};
+`;
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+const ValuationContainer = styled.div`
+  height: 64px;
+  padding: 8px 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${designSystem.color.neutral.gray800};
+  border-radius: 8px;
+  ${designSystem.font.title5};
+  color: ${designSystem.color.neutral.gray400};
+`;
+
+const CurrentValuation = styled.div`
+  display: flex;
+  gap: 4px;
+  align-items: center;
+  ${designSystem.font.title3};
+  color: ${designSystem.color.neutral.gray600};
+
+  > span {
+    ${designSystem.font.title2};
+    color: ${designSystem.color.neutral.white};
+  }
+`;
