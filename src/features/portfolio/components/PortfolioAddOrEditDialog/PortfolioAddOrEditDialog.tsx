ed6e1dc72b85093f6f940363a91333d7475fcf0c
Jay <chlwodnjs34@gmail.com>
2024-11-28 21:50:55 +0900

    #24 feat: Portfolio Dialog 관련 컴포넌트들 추가

---
+++ src/features/portfolio/components/PortfolioAddOrEditDialog/PortfolioAddOrEditDialog.tsx
@@ -0,0 +1,20 @@
+import { PortfolioDetails } from "@/features/portfolio/api/types";
+import useResponsiveLayout from "@/hooks/useResponsiveLayout";
+import PortfolioAddOrEditDialogD from "./desktop/PortfolioAddOrEditDialogD";
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+  portfolioDetails?: PortfolioDetails;
+};
+
+export default function PortfolioAddOrEditDialog(props: Props) {
+  const { isMobile, isDesktop } = useResponsiveLayout();
+
+  return (
+    <>
+      {isDesktop && <PortfolioAddOrEditDialogD {...props} />}
+      {/* {isMobile && <PortfolioAddOrEditDialogM {...props} />} */}
+    </>
+  );
+}
