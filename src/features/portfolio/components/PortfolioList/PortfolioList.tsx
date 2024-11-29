002fb53d597a398c41ea5bc33e2dd170d04466c3
Jay <chlwodnjs34@gmail.com>
2024-11-29 16:51:28 +0900

    #26 feat: Portfolio List 추가

---
+++ src/features/portfolio/components/PortfolioList/PortfolioList.tsx
@@ -0,0 +1,61 @@
+import { IconButton } from "@/components/Buttons/IconButton";
+import usePortfolioListQuery from "@/features/portfolio/api/queries/usePortfolioListQuery";
+import useResponsiveLayout from "@/hooks/useResponsiveLayout";
+import designSystem from "@/styles/designSystem";
+import { useBoolean } from "@fineants/demolition";
+import styled from "styled-components";
+import PortfolioAddDialog from "../PortfolioAddOrEditDialog/PortfolioAddOrEditDialog";
+import PortfolioListTable from "./desktop/PortfolioListTable";
+
+export default function PortfolioList() {
+  const { isDesktop, isMobile } = useResponsiveLayout();
+
+  const { data } = usePortfolioListQuery();
+
+  const {
+    state: isAddPortfolioDialogOpen,
+    setTrue: onAddPortfolioButtonClick,
+    setFalse: onAddPortfolioDialogClose,
+  } = useBoolean();
+
+  const isEmpty = data.length === 0;
+
+  return (
+    <>
+      <Header $isMobile={isMobile}>
+        <h1>내 포트폴리오</h1>
+        {isMobile && !isEmpty && (
+          <IconButton
+            icon="folder-add"
+            size="h40"
+            iconColor="custom"
+            customColor={{ color: "white", hoverColor: "blue400" }}
+            bgColor="blue500"
+            onClick={onAddPortfolioButtonClick}
+          />
+        )}
+      </Header>
+
+      {isDesktop && <PortfolioListTable data={data} />}
+      {/* {isMobile && <PortfolioListCardTable data={data} />} */}
+
+      <PortfolioAddDialog
+        isOpen={isAddPortfolioDialogOpen}
+        onClose={onAddPortfolioDialogClose}
+      />
+    </>
+  );
+}
+
+const Header = styled.header<{ $isMobile: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: ${({ $isMobile }) => ($isMobile ? "32px 16px 16px" : "0 0 40px")};
+
+  h1 {
+    ${({ $isMobile }) =>
+      $isMobile ? designSystem.font.heading3 : designSystem.font.heading2};
+    color: ${designSystem.color.neutral.gray900};
+  }
+`;
